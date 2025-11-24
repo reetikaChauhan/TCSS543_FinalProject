f55ca68652b532aee6abcf7170936bb8a5d9fb2e
RCWARF <165970782+RCWARF@users.noreply.github.com>
2025-11-24 14:17:42 -0800

    Made graph injestion code to injest txt outputs and make an adjacency dictionary as a datastructure in python

---
+++ Graphinjest.py
@@ -0,0 +1,59 @@
+'''
+ Max-Flow(FordFulkerson)
+ Initially f(e)=0 for all e in G
+ While there is an s-t path in the residual graph Gf
+ Let P be a simple s-t path in Gf
+ f =augment(f, P)
+ Update f to be f'
+ Update the residual graph Gf to be Gf'
+ Endwhile
+ Return f
+ '''
+'''
+augment(f, P)
+ Let b=bottleneck(P, f)
+ For each edge (u,v)∈P
+   If e=(u,v) is a forward edge then
+     increase f(e) in G by b
+   Else ((u, v) is a backward edge, and let e=(v,u))
+     decrease f(e) in G by b
+ Endif
+ Endfor
+ Return(f)
+'''
+def load_graph(path):
+    graph = {}
+
+    with open(path, "r") as f:
+        for line_num, line in enumerate(f, start=1):
+            parts = line.strip().split()
+            if len(parts) != 3:
+                raise ValueError(
+                    f"Error on line {line_num}: '{line.strip()}'\n"
+                    f"Expected format: <u> <v> <value>"
+                )
+
+            u, v, val = parts[0], parts[1], int(parts[2])
+
+            if u not in graph:
+                graph[u] = {}
+            graph[u][v] = val
+    return graph
+
+def print_graph(graph):
+    print("\nGraph Adjacency List\n")
+    for u in graph:
+        print(f"{u} -> {graph[u]}")
+    print("\n")
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: Graphinjest.py <input_file>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+    graph = load_graph(path)
+    print_graph(graph)
